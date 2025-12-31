7e5d2b37f1c5d32590e1352b05d2c3136a919aee
sun <suunn001@gmail.com>
2025-12-31 21:59:11 +0900

    ✨ feat: 스캔 범위 지정

--- app-api/src/main/java/com/finance/app_api/AppApiApplication.java
+++ app-api/src/main/java/com/finance/app_api/AppApiApplication.java
@@ -2,7 +2,11 @@ package com.finance.app_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "com.finance.infra.entity")
+@EnableJpaRepositories(basePackages = "com.finance.infra.repository")
 @SpringBootApplication(scanBasePackages = "com.finance") // 모든 모듈의 공통 패키지 경로
 public class AppApiApplication {
 
